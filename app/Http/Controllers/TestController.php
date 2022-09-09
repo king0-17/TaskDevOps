cf747a7e570b97feffd48e97e50bbc417ecf7438
Jobnico <108317753+Jobnico@users.noreply.github.com>
2022-09-09 15:01:47 +0800

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class TestController extends Controller
+{
+    public function __construct()
+    {
+        $this->redis = Redis::connection();
+    }
+    public function set()
+    {
+        $this->redis->set('user_details', json_encode([
+                'first_name' => 'Alex', 
+                'last_name' => 'Malakas'
+            ])
+        );
+    }
+
+    public function get()
+    {
+        $response = $this->redis->get('user_details');
+
+        $response = json_decode($response);
+
+        return response()->json([
+            'data' => $response
+        ]);
+    }
+}
